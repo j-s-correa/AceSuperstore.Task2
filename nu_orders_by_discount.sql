9834398cf8562d372107371d87ba8d7cbc7dd9eb
j-s-correa <129101287+j-s-correa@users.noreply.github.com>
2025-07-11 22:52:53 +0100

    Delete nu_orders_by_discount.sql

--- nu_orders_by_discount.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    fs.discount_percent,
-    COUNT(*) AS number_of_orders,
-    AVG(fs.profit_per_unit) AS avg_profit_per_unit,
-    AVG(fs.margin_percent) AS avg_margin
-FROM fact_sales fs
-GROUP BY fs.discount_percent
-ORDER BY fs.discount_percent;
