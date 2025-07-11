3c8ec4a9281d7d9fbacf29bdf266f00f3ca272cd
j-s-correa <129101287+j-s-correa@users.noreply.github.com>
2025-07-11 22:52:43 +0100

    Delete insert_datetable.sql

--- insert_datetable.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO dim_date (order_date, day, month, quarter, year)
-SELECT DISTINCT
-    CAST([Order_Date] AS DATE),
-    DAY(CAST([Order_Date] AS DATE)),
-    MONTH(CAST([Order_Date] AS DATE)),
-    DATEPART(QUARTER, CAST([Order_Date] AS DATE)),
-    YEAR(CAST([Order_Date] AS DATE))
-FROM dbo.AceSuperstore
-WHERE [Order_Date] IS NOT NULL;
