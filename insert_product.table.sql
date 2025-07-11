8bba50f70563a5d73e60e9830f169760db3f0b3d
j-s-correa <129101287+j-s-correa@users.noreply.github.com>
2025-07-11 22:54:07 +0100

    Delete insert_product.table.sql

--- insert_product.table.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO dim_product (product_id, product_name, plain_category, category, sub_category)
-SELECT DISTINCT 
-    [Product_ID],
-    [Product_Name],
-    [Plain_Category],
-    [Category],
-    [Sub_Category]
-FROM dbo.AceSuperstore
-WHERE [Product_ID] IS NOT NULL;
