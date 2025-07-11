c4f22bd53785dd4e896ca66e15749db3aaf1921f
j-s-correa <129101287+j-s-correa@users.noreply.github.com>
2025-07-11 22:52:21 +0100

    Delete top10_revenue.sql

--- top10_revenue.sql
+++
@@ -1,9 +0,0 @@
-SELECT TOP 10
-    c.customer_id,
-    c.country,
-    SUM(fs.total_revenue) AS lifetime_revenue,
-    COUNT(DISTINCT fs.order_id) AS orders_made
-FROM fact_sales fs
-JOIN dim_customer c ON fs.customer_id = c.customer_id
-GROUP BY c.customer_id, c.country
-ORDER BY lifetime_revenue DESC;
